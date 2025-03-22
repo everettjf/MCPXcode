12053df5018c6ddd7a918f0e3f7e718eb8fcaeb7
everettjf <everettjf@live.com>
2025-03-21 18:36:44 -0700

    basic

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from mcpxcode!")
+from typing import Any
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("MCPXcode")
+
+@mcp.tool()
+async def get_mcpxcode() -> str:
+    """Print Summary
+    """
+    return "Hello MCPXcode :) MCPXcode is MCP server for Xcode."
 
 
 if __name__ == "__main__":
-    main()
+    mcp.run(transport='stdio')
